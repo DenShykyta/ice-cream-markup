0a44f8dce6d002c3e58f4aaa10af44c6b7ecb8d1
DenShykyta <wrkacc4@gmail.com>
2024-01-19 22:14:54 +0200

    Close the mobile menu when clicking on a link
    Реалізовано:
    - плавний скрол сторінки при натисканні на посилання навігаціного меню в хедері сторінки. Зафіксувано Хедер при скролі.
    - вимкнено скрол сторінки під відкритим мобільним меню;
    - реалізовано закриття моб. меню при натисканні на посилання;
    Ресурси і матеріали:  FreelancerLS gide YouTube

--- src/js/scroll-nav.js
+++ src/js/scroll-nav.js
@@ -1,6 +1,6 @@
 // Прокрутка при натисканні на навігаційні посилання в хедері сторінки
 
-const menuLinks = document.querySelectorAll('.nav-link[data-goto]');
+const menuLinks = document.querySelectorAll('.link[data-goto]');
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
     menuLink.addEventListener('click', onMenuLinkClick);
@@ -17,6 +17,13 @@ if (menuLinks.length > 0) {
         pageYOffset -
         document.querySelector('header').offsetHeight;
 
+      // --------Закриття мобільного меню при натисканні на посилання -------//
+      const mobileMenu = document.querySelector('.js-menu-container');
+      if (mobileMenu.classList.contains('is-open')) {
+        document.body.classList.remove('lock');
+        mobileMenu.classList.toggle('is-open');
+      }
+      // ------------------------------------------------------------------- //
       window.scroll({
         top: gotoBlockValue,
         behavior: 'smooth',
