7b48160b8a69878908b49cd95e8b30557645759a
DenShykyta <wrkacc4@gmail.com>
2024-01-19 21:08:14 +0200

    add smoof scroll to header nav-menu
    FreelancerLS  gide YouTube

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'slick-carousel';
 import './js/modal';
+import './js/scroll-nav';
 // import './js/mobile-menu';
 
 $('.carousel').slick({ dots: true });
---
+++ src/js/scroll-nav.js
@@ -0,0 +1,27 @@
+// Прокрутка при натисканні на навігаційні посилання в хедері сторінки
+
+const menuLinks = document.querySelectorAll('.nav-link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector('header').offsetHeight;
+
+      window.scroll({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
